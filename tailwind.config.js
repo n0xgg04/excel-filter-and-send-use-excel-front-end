a4e0b46884bf4d86854bca23e2dadf9eb1d5ae2f
n0xgg04 <dulieu.vblc@gmail.com>
2023-06-20 01:23:23 +0700

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.jsx', './src/**/*.js', "./src/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
